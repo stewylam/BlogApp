e33f4a4d14fca04c3350be9341d90b75291fe8d9
Stephanie Lam <stephaniewylam@hotmail.com>
2017-06-06 14:43:36 +0200

    login information saved in pg

--- app.js
+++ app.js
@@ -1,29 +1,96 @@
 const fs = require('fs')
 const express = require('express')
 const pg = require ('pg')
+const bodyParser = require('body-parser')
+const session = require('express-session');
+const cookieParser = require('cookie-parser')
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + '@localhost/blogapp');
+
 
 const app = express();
 
-const bodyParser = require('body-parser')
-
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.urlencoded({extended: true})); // get information from html forms
 app.use(express.static('public'));
+app.use(session({
+    secret: 'security',
+    resave: true,
+    saveUninitialized: false
+}));
 
-var connectionString = 'postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + '@localhost/bulletinboard';
-// connection string. Anyone can use this app, no need for username and password.
+const User = sequelize.define('user', {
+  username: Sequelize.STRING,
+  password: Sequelize.STRING
+});
+
+//create tables -- if tables exists, then leave it
+sequelize.sync()
+      
+
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html')
+});
+
+app.post('/', (req, res) => {
+    const username = req.body.user
+    const password = req.body.pass
+    User.create({
+        username: username,
+        password: password
+    })
+    .then( () => {
+        res.redirect('/')
+    })
+})
+
 
 const server = app.listen(8080, () => {
     console.log('server has started at ', server.address().port)
 }); // application is listens to the request. And everytime the browser goes to localhost:8080 it will print out "Server has started at".
 
 
-app.get("/" (req, res) => {
-
-    res.sendFile('index.html')
-})
-
 /*
-app.post("/profile" (req, res) => {
+app.post('/blog', (req, res) => {
+
+    var user = request.session.user;
+    if (user === undefined) {
+        response.redirect('/?message=' + encodeURIComponent("Please log in to view your profile."));
+    } else {
+        response.render('profile', {
+            user: user
+        });
+    }
+
+
+    var username = req.body.user;
+    var pass = req.body.pass;
+
+// the application will connect to the sql database
+    pg.connect(connectionString, function(err, client, done) {
+
+     client.query('insert into users (username, pass) values (\'' + username + '\', \'' + pass +'\')', function(err) {
+            if(err) {
+            throw err;
+        }
+     }); // the database is connected. The values which are inserted in the form will be inserted to the SQL messages table.
+
+        done();
+        res.sendFile('blog.html'); // after submitting the page gets redirected to the bulletinboard
+    });
+
+});*/
+/*    if (req.body.user ) {
+        alert('logged in succesfully')
+        res.sendFile('blog.html')
+    } else {
+        res.redirect('/')
+        alert('username / password incorrect');
+    }*/
+
+
+/*app.post('/profile', (req, res) => {
 
     //check session
 
@@ -31,11 +98,12 @@ app.post("/profile" (req, res) => {
             res.end("not logged in")
         //if logged in
 
-            res.render(profile page {specifdata for user:})
+            res.render('profile', {specifdata for user:})
 
 })
+*/
 
-app.post("showmymessages", (req, res) => {
+/*app.post("showmymessages", (req, res) => {
     
     //check session
         //if not logged in
